3a2692e55b6ce0bb94630e38ec64874eb540ecfc
sa-borgia3 <sa.borgia3@gmail.com>
2021-02-12 15:47:16 +0100

    input upload file scaffolding

--- laravel-auth/resources/views/home.blade.php
+++ laravel-auth/resources/views/home.blade.php
@@ -1,23 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
+                <div class="card">
+                    <div class="card-header">Upload Image!!</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <form action="#">
+                            <div class="form-group">
+                                <input type="file" class="form-control border-0" name="" enctype="">
+                                <input type="submit" class="btn btn-primary" value="Upload">
+                                <input type="submit" class="btn btn-danger" value="Clear">
+                            </div>
+                        </form>
+
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
